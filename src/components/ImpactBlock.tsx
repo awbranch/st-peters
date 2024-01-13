0aeb08c278f36f3e36fc61b25a9950710305a34f
Andrew Branch <awbranch@gmail.com>
2024-01-13 17:09:42 -0500

    Fix coloring on the impact block

--- src/components/ImpactBlock.tsx
+++ src/components/ImpactBlock.tsx
@@ -17,9 +17,7 @@ export default function ImpactBlock({ id, title, text, impacts }: Props) {
         <dl className="grid grid-cols-1 gap-x-8 gap-y-16 text-center lg:grid-cols-3">
           {impacts.map((impact, i) => (
             <div key={i} className="mx-auto flex max-w-xs flex-col gap-y-4">
-              <dt className="text-base leading-7 text-gray-300">
-                {impact.category}
-              </dt>
+              <dt className="text-base leading-7">{impact.category}</dt>
               <dd className="order-first text-4xl font-semibold tracking-wide sm:text-6xl">
                 {impact.value.toLocaleString('en-US')}
               </dd>
