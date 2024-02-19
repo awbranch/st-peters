1ee481a2f3133009785b8e7662584b5b7da8ef6a
Andrew Branch <awbranch@gmail.com>
2024-02-19 18:14:23 -0500

    Updated the styling to make sure the footer always sits at the bottom of the page.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -38,7 +38,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en" className="scroll-smooth">
+    <html lang="en">
       <body className={twJoin(montserrat.className, 'text-black text-base')}>
         <Header />
         <main>{children}</main>
