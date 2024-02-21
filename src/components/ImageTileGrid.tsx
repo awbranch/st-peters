0d45a7cce3d286a1a7b4aed884f5987b2384552d
Andrew Branch <awbranch@gmail.com>
2024-02-20 20:51:10 -0500

    Redesigned the ImageTileGrid to be lighter.

--- src/components/ImageTileGrid.tsx
+++ src/components/ImageTileGrid.tsx
@@ -5,30 +5,25 @@ import Link from 'next/link';
 
 export default async function ImageTileGrid({ images }: Props) {
   return (
-    <div className="mx-auto grid max-w-2xl auto-rows-fr grid-cols-1 gap-10 lg:mx-0 lg:max-w-none lg:grid-cols-3">
+    <ul
+      role="list"
+      className="mx-auto grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-x-10 gap-y-20"
+    >
       {images.map((tile) => (
-        <article
-          key={tile._key}
-          className="relative isolate flex flex-col justify-end overflow-hidden rounded-2xl bg-gray-900 px-8 pb-8 pt-80 sm:pt-48 lg:pt-80  group"
-        >
-          <ResponsiveImage
-            image={tile.image}
-            priority={false}
-            sizes={`100vw`}
-            className="absolute inset-0 -z-10 h-full w-full object-cover"
-          />
-
-          <div className="absolute inset-0 -z-10 bg-gradient-to-t from-gray-900 via-gray-900/20 group-hover:opacity-80" />
-          <div className="absolute inset-0 -z-10 rounded-2xl ring-1 ring-inset ring-gray-900/10" />
-
-          <h3 className="mt-3 text-lg font-semibold leading-6 text-white">
-            <Link href={tile.url}>
-              <span className="absolute inset-0" />
+        <li key={tile._key}>
+          <Link href={tile.url} className={'group'}>
+            <ResponsiveImage
+              image={tile.image}
+              priority={false}
+              sizes={`100vw`}
+              className="aspect-square w-full rounded-2xl object-cover"
+            />
+            <h3 className="mt-4 text-lg font-semibold leading-8 text-center underline-offset-4 group-hover:underline">
               {tile.label}
-            </Link>
-          </h3>
-        </article>
+            </h3>
+          </Link>
+        </li>
       ))}
-    </div>
+    </ul>
   );
 }
