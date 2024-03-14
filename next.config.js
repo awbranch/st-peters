d326641b044f63b2a5c3f169d312cfced80c034a
Andrew Branch <awbranch@gmail.com>
2024-03-14 11:58:13 -0400

    Added redirects from old site links.

--- next.config.js
+++ next.config.js
@@ -15,6 +15,50 @@ const nextConfig = {
       fullUrl: true,
     },
   },
+  async redirects() {
+    return [
+      {
+        source: '/contact-us',
+        destination: '/contact',
+        permanent: true,
+      },
+      {
+        source: '/other-programs',
+        destination: '/about/area-programs',
+        permanent: true,
+      },
+      {
+        source: '/meal-program',
+        destination: '/about/lunch-program',
+        permanent: true,
+      },
+      {
+        source: '/meal-program',
+        destination: '/about/lunch-program',
+        permanent: true,
+      },
+      {
+        source: '/community-garden/',
+        destination: '/about/seasonal-programs',
+        permanent: true,
+      },
+      {
+        source: '/community-garden',
+        destination: '/about/seasonal-programs',
+        permanent: true,
+      },
+      {
+        source: '/delivery-volunteers',
+        destination: '/volunteer',
+        permanent: true,
+      },
+      {
+        source: '/news-events',
+        destination: '/news',
+        permanent: true,
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
